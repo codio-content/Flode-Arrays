e7b19b7263bc3d5b7796db2aa9b24362c6cd89c1
codio-units <spackham@codio.com>
2016-01-29 15:07:11 +0000

    elliott: fixed the numsearch.js test case

--- numsearch.js
+++ numsearch.js
@@ -13,17 +13,17 @@ for(var i = 0; i < random; i ++) {
   input.push(old)
 }
 
-var out = testing.RunGraphWithInputs(script, [input, input[index]]);
-var out = testing.RunGraphWithInputs(script, [input, 9999]);
+var out1 = testing.RunGraphWithInputs(script, [input, input[index]]);
+var out2 = testing.RunGraphWithInputs(script, [input, 9999]);
 
 // console.log(out)
 
-if(out.length == 0) {
+if(out1.length == 0 || out2.length == 0) {
   console.log("Your program isn't outputting anything.")
   process.exit(1)
 }
 
-if(out.length == 2 && out[0] == index && out[1] == -1) {
+if(out1[0] == index && out2[0] == -1) {
   console.log('Well done!')
   process.exit(0)
 }
